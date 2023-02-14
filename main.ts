1bc5dd34983ae9d3c4396bc21c0d32d7fe5fb45a
Sainz31 <122534761+Sainz31@users.noreply.github.com>
2023-02-14 13:57:27 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    X += 1
+})
 input.onButtonPressed(Button.AB, function () {
-    radio.sendString("1")
+    radio.setGroup(X + Y)
 })
 radio.onReceivedString(function (receivedString) {
     led.plot(parseFloat(receivedString.substr(0, 1)), parseFloat(receivedString.substr(1, 2)))
     basic.pause(3000)
     basic.clearScreen()
 })
+input.onButtonPressed(Button.B, function () {
+    Y += 1
+})
+let X = 0
+let Y = 0
+Y = 0
+X = 0
 radio.setGroup(1)
